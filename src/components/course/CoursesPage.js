978a35c326051fb63489c671141cb2fe442af8f0
Bikash <bikash@datarpm.com>
2016-07-19 22:09:57 +0530

    modified:   src/actions/courseActions.js 	modified:   src/components/course/CoursesPage.js 	new file:   src/components/course/courseForm.js 	new file:   src/components/course/courseList.js 	new file:   src/components/course/courseListRow.js 	new file:   src/components/course/manageCoursePage.js 	modified:   src/reducers/courseReducer.js 	modified:   src/routes.js

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React,{PropTypes} from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import {bindActionCreators} from 'redux';
+import CourseList from './courseList';
 
 class CoursesPage extends React.Component{
   constructor(props,context){
@@ -14,11 +15,12 @@ class CoursesPage extends React.Component{
   }
 
   render(){
+    const {courses} = this.props;
     debugger;
     return(
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses}/>
       </div>
     );
   }
